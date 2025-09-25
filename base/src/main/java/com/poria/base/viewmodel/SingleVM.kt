08885613ff170cf06fe2534eb5d9fca5f8d0ce11
PPPoria <1316543327@qq.com>
2025-09-25 09:49:31 +0800

    更新了base的SingleVM，现在支持StateFlow

--- base/src/main/java/com/poria/base/viewmodel/SingleVM.kt
+++ base/src/main/java/com/poria/base/viewmodel/SingleVM.kt
@@ -2,9 +2,16 @@ package com.poria.base.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SingleVM<T>: ViewModel() {
     val cur: MutableLiveData<T> by lazy {
         MutableLiveData<T>()
     }
+    private val mutableFlow: MutableStateFlow<T> by lazy {
+        MutableStateFlow(cur.value!!)
+    }
+    val flow = mutableFlow.asStateFlow()
 }
